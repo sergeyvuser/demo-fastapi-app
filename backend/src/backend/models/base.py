a73e986a5e9fb4fbb1f475b01336459e4a44950b
sergeyv <99827892+sergeyvuser@users.noreply.github.com>
2026-07-09 09:42:53 +0300

    chore(backend): add naming convention for models

--- backend/src/backend/models/base.py
+++ backend/src/backend/models/base.py
@@ -1,10 +1,16 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 
+from backend.core.config import settings
 from backend.utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
