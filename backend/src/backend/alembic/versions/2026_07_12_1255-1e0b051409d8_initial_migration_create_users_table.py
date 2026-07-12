7292620edbf17de753f301c1079b5700fcd10478
sergeyv <99827892+sergeyvuser@users.noreply.github.com>
2026-07-12 13:09:50 +0300

    feat(backend): initial migration — users table

---
+++ backend/src/backend/alembic/versions/2026_07_12_1255-1e0b051409d8_initial_migration_create_users_table.py
@@ -0,0 +1,39 @@
+"""Initial migration- create users table
+
+Revision ID: 1e0b051409d8
+Revises: 
+Create Date: 2026-07-12 12:55:24.183872
+
+"""
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = '1e0b051409d8'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('users',
+    sa.Column('id', sa.Uuid(), nullable=False),
+    sa.Column('username', sa.String(length=32), nullable=False),
+    sa.Column('email', sa.String(length=255), nullable=False),
+    sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    sa.Column('is_active', sa.Boolean(), server_default='true', nullable=False),
+    sa.Column('is_superuser', sa.Boolean(), server_default='false', nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.Column('updated_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.PrimaryKeyConstraint('id', name=op.f('pk_users')),
+    sa.UniqueConstraint('email', name=op.f('uq_users_email')),
+    sa.UniqueConstraint('username', name=op.f('uq_users_username'))
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('users')
